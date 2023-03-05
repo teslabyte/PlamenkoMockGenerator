0d3b3a66e9387086bc848053f98b7673901d26c6
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 16:05:57 +0100

    Created a class that generates random address

---
+++ Generator/AddressGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlamenkoMockGenerator.Generator
+{
+    internal class AddressGenerator
+    {
+        string[] streets;
+        public AddressGenerator(string[] streets) {
+            this.streets = streets;
+        }
+
+        public string GenerateRandomAddress()
+        {
+            Random rnd = new Random();
+            int streetIndex = rnd.Next(0, streets.Length);
+            int streetNumber = rnd.Next(1, 150);
+
+            return streets[streetIndex] + " " + streetNumber;
+        }
+    }
+}
