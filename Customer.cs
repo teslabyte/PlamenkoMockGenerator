6cb31d0df182abe9c43fbfa5c61d638bfec4cfe6
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 17:46:48 +0100

    Created classes which i base the mock data on

---
+++ Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace PlamenkoMockGenerator
+{
+    internal class Customer
+    {
+        public long customerId { get; set; }
+        public string customerName { get; set; }
+        public string customerAddress { get; set; }
+        public string customerPhoneNumber { get; set; }
+
+        public List<CustomerOrder> customerOrders { get; set; }
+        public string customerAdditionalInfo { get; set; }
+    }
+}
---
+++ CustomerOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace PlamenkoMockGenerator
+{
+    internal class CustomerOrder
+    {
+        public string orderedDate { get; set; }
+        public string deliveredDate { get; set; }
+        public string additionalOrderInfo { get; set; } = "-";
+
+    }
+}
