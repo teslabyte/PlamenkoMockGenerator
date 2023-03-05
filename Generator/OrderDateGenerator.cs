a2a5097a87a3e903b1fdfbaaf65b904052df93f1
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 17:03:43 +0100

    Created a class that generates random dates for customer orders

---
+++ Generator/OrderDateGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace PlamenkoMockGenerator.Generator
+{
+    internal class OrderDateGenerator
+    {
+        public string[] GenerateOrderDates() {
+            Random rnd = new Random();
+            int day = rnd.Next(1, 30);
+            int month = rnd.Next(1, 12);
+            int year = rnd.Next(2020, 2023);
+            string orderedDate = day + "." + month + "." + year;
+
+            int del = rnd.Next(1, 10);
+            if(del > 8)
+            {
+                return new string[] { orderedDate, "-" };
+            }
+            else
+            {
+                day = rnd.Next(day, 30);
+                month= rnd.Next(month, 12);
+                year = rnd.Next(year, 2024);
+                string deliveredDate = day + "." + month + "." + year;
+                return new string[] { orderedDate, deliveredDate };
+            }
+        }
+    }
+}
