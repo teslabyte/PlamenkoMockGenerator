2e108fe8b5e0fe81eed9b9cdd253c0581a4c2eff
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 15:40:03 +0100

    Created a class that generates random phone number

---
+++ Generator/PhoneNumberGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlamenkoMockGenerator.Generator
+{
+    internal class PhoneNumberGenerator
+    {
+        string[] phoneNumberPrefixes = { "060", "061","062","063","064", "065", "066", "069" };
+
+        int[] generateNumbers()
+        {
+            Random rnd = new Random();
+            int first = rnd.Next(100, 999);
+            int second = rnd.Next(100, 9999);
+            int prefix = rnd.Next(0,8);
+            return new int[] { prefix, first, second }; 
+        }
+        
+        public string getRandomPhoneNumber()
+        {
+            int[] generatedRandom = generateNumbers();
+
+            return phoneNumberPrefixes[generatedRandom[0]] + "-" + generatedRandom[1] + "-" + generatedRandom[2];
+        }
+    }
+}
