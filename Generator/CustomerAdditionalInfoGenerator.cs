c2fa124c84cab5323ab38ecda8622e4fcb6c3c0a
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 16:44:14 +0100

    Created a class that generates basic customer additional information

---
+++ Generator/CustomerAdditionalInfoGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlamenkoMockGenerator.Generator
+{
+    internal class CustomerAdditionalInfoGenerator
+    {
+        string additionalInfoTemplateTemp = "Isporuka izmedju ";
+
+        public string GenerateCustomerAdditionalInfo()
+        {
+            Random rnd = new Random();
+            int before = rnd.Next(7, 13);
+            int after = rnd.Next(13, 22);
+
+            return additionalInfoTemplateTemp + before + ".00 - " + after + ".00";
+        }
+    }
+}
