14fe9bc5ef5d7a3f24bc868147decf5b7bfeec4d
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-05 15:46:51 +0100

    Created a class that generates random full name

---
+++ Generator/FullNameGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlamenkoMockGenerator.Generator
+{
+    internal class FullNameGenerator
+    {
+        string[] names;
+        string[] surnames;
+        public FullNameGenerator(string[] names, string[] surnames) {
+            this.names = names;
+            this.surnames = surnames;
+        }
+
+        public string GetRandomFullName()
+        {
+            Random rnd = new Random();
+            int nameIndex = rnd.Next(0, names.Length);
+            int surnameIndex = rnd.Next(0, surnames.Length);
+            
+            return names[nameIndex] + " " + surnames[surnameIndex];
+        }
+    }
+}
