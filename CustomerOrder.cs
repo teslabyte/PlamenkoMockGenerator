5b321a7add917f6da0425d3d141cee3579dbb0a0
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-11 18:03:06 +0100

    Added orderId, orderName, orderQuantity fields to CustomerOrder

--- CustomerOrder.cs
+++ CustomerOrder.cs
@@ -2,11 +2,13 @@
 {
     internal class CustomerOrder
     {
+        public long orderId { get; set; }
         public string orderedDate { get; set; }
         public string deliveredDate { get; set; }
         public string additionalOrderInfo { get; set; } = "-";
-
         public string paidDate { get; set; }
+        public string orderName { get; set; }  //Example: Spacva pelet,Biostar pelet,...
+        public string orderQuantity { get; set; }  //Example: 2t, 3 palletes, 5m,....
 
     }
 }
