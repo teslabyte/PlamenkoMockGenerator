9805d0b813eb7b5cd726f541321a0bd45b018e6f
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-11 17:27:11 +0100

    Create a class that generates order quanity amount

---
+++ Generator/OrderQuantityGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace PlamenkoMockGenerator.Generator
+{
+    internal class OrderQuantityGenerator
+    {
+        private string[] quantityUnit = { "t", "paleta" };
+
+        public string GenerateOrderQuantity()
+        {
+            Random rnd = new Random();
+            int quantity = rnd.Next(0, 6);
+            int unit = rnd.Next(0, 3);
+            if(unit == 1) return quantity + quantityUnit[0];
+            else return quantity + " " + quantityUnit[1];
+        }
+    }
+}
